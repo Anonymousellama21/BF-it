a96d415505e149985c9e93602b723ff6e7c445c8
Anonymousellama21 <49965107+Anonymousellama21@users.noreply.github.com>
2022-06-14 13:16:44 -0400

    Update Minify.py
    Added a more complex case.
    Example of the new case:
    <+>-< becomes -<+
    <[-]>+++< becomes +++<[-]

--- Compiler/Minify.py
+++ Compiler/Minify.py
@@ -1,3 +1,18 @@
+import re
+
+def brackets_matched(string, open='[', close=']'):
+    depth = 0
+    for c in string:
+        if c == open:
+            depth += 1
+        elif c == close:
+            if depth == 0:
+                return False
+            depth -= 1
+
+    return depth == 0
+
+
 def minify(code):
     old_code = ""
 
@@ -10,5 +25,16 @@ def minify(code):
         code = code.replace("-+", "")
 
         code = code.replace("][-]", "]")
+
+        code = code.replace("[]", "") #TODO: check if this is safe. it may break something like a for(;;) depending on how that compiles
+
+        i = 0
+        while i < len(code) and (match := re.search(r"<([\-\+\[\]]+)>([\-\+\[\]]+)<", code[i:])):
+            if brackets_matched(match.group(1)) and brackets_matched(match.group(2)):
+                code = code[:i + match.start()] + match.group(2) + "<" + match.group(1) + code[i + match.end():]
+                i += match.end() - 2
+
+            else:
+                i += match.end()
 
     return code
