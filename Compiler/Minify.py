dc5eb22d2a69cb6b27baa6bfe1f344259cd8b953
Anonymousellama21 <49965107+Anonymousellama21@users.noreply.github.com>
2022-06-15 01:18:05 -0400

    Update Minify.py
    fix new case for minify()
    add strip_unnecessary()

--- Compiler/Minify.py
+++ Compiler/Minify.py
@@ -10,7 +10,7 @@ def brackets_matched(string, open='[', close=']'):
                 return False
             depth -= 1
 
-    return depth == 0
+    return (depth == 0)
 
 
 def minify(code):
@@ -28,13 +28,102 @@ def minify(code):
 
         code = code.replace("[]", "") #TODO: check if this is safe. it may break something like a for(;;) depending on how that compiles
 
+
         i = 0
-        while i < len(code) and (match := re.search(r"<([\-\+\[\]]+)>([\-\+\[\]]+)<", code[i:])):
-            if brackets_matched(match.group(1)) and brackets_matched(match.group(2)):
-                code = code[:i + match.start()] + match.group(2) + "<" + match.group(1) + code[i + match.end():]
-                i += match.end() - 2
+        while not code[i] in "<>":
+            i += 1
+        match_start = i
+        match_middle = 0
+        while i < len(code):
+            while i < len(code) and not code[i] in "<>":
+                i += 1
+
+            if i == len(code):
+                break
+
+            if brackets_matched(code[match_start+1:i]) and code[i] == "<>"[code[match_start]=='<']:
+                match_middle = i
+            else:
+                match_start = i
+                i += 1
+                continue
+
+            i += 1
+
+            while not code[i] in "<>":
+                i += 1
+
+            if i == len(code):#FIXME: maybe misses something on the end
+                break
+
+            if brackets_matched(code[match_middle+1:i]) and code[i] == code[match_start]:
+                print(code[match_middle+1:i] + code[match_start] + code[match_start+1:match_middle])
+                code = code[:match_start] + code[match_middle+1:i] + code[match_start] + code[match_start+1:match_middle] + code[i+1:]
+
+                i = 0
+                while not code[i] in "<>":
+                    i += 1
+                continue
 
             else:
-                i += match.end()
+                match_start = i
+                i += 1
+
+    return code
+
+def strip_unnecessary(code):
+
+    #remove code from the last print to the end
+
+    safe_end = len(code)
+
+    i = len(code)-1
+    depth = 0
+    while i >= 0:
+        if code[i] == ']':
+            depth += 1
+            i -= 1
+            continue
+
+        elif code[i] == '[':
+            depth -= 1
+            i -= 1
+            continue
+
+        elif code[i] == '.':
+            break
+
+        if depth == 0:
+            safe_end = i
+
+        i -= 1
+
+    code = code[:safe_end]
+
+
+    #remove code from the beginning
+
+    dot_count = 0
+
+    i = 0
+    depth = 0
+    while i < len(code):
+        if code[i] == '[':
+            depth += 1
+        elif code[i] == ']':
+            depth -= 1
+
+        if depth == 0:
+
+            if code[i] == '.':
+                dot_count += 1
+
+            #end at the first io
+            if code[i] in ",+-":
+                break
+
+        i += 1
+
+    code = "."*dot_count + code[i:]
 
     return code
